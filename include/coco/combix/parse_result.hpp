a5c2fc219c69127fb5e6c9623486737fa691c0ad
agatan <m.k.mahoo@gmail.com>
2016-04-20 18:57:54 +0900

    Add parse_result.hpp

---
+++ include/coco/combix/parse_result.hpp
@@ -0,0 +1,18 @@
+#ifndef COCO_COMBIX_PAESE_RESULT_HPP
+#define COCO_COMBIX_PAESE_RESULT_HPP
+
+#include <coco/combix/error.hpp>
+#include <coco/expected.hpp>
+
+namespace coco {
+  namespace combix {
+
+    template <typename T, typename S>
+    using parse_result =
+        coco::expected<T, parse_error<typename stream_trait<S>::value_type>>;
+
+
+  } // namespace combix
+} // namespace coco
+
+#endif
--- include/coco/combix/primitives.hpp
+++ include/coco/combix/primitives.hpp
@@ -2,16 +2,12 @@
 #define COCO_COMBIX_PRIMITIVES_HPP_
 
 #include <coco/combix/error.hpp>
+#include <coco/combix/parse_error.hpp>
 #include <coco/combix/stream_trait.hpp>
-#include <coco/combix/unused.hpp>
 #include <coco/expected.hpp>
 
 namespace coco {
   namespace combix {
-
-    template <typename T, typename S>
-    using parse_result =
-        coco::expected<T, parse_error<typename stream_trait<S>::value_type>>;
 
     struct any_parser {
       template <typename Stream>
