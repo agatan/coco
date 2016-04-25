edab4a298b4c582c27433d3aa4fbd2e573c4af94
agatan <m.k.mahoo@gmail.com>
2016-04-25 11:39:30 +0900

    Add wrapped parser type: parser<Result, Stream>

---
+++ include/coco/combix/parser.hpp
@@ -0,0 +1,69 @@
+#ifndef COCO_COMBIX_PARSER_HPP_
+#define COCO_COMBIX_PARSER_HPP_
+
+#include <memory>
+
+#include <coco/combix/parse_result.hpp>
+
+namespace coco {
+  namespace combix {
+
+    template <typename Result, typename Stream>
+    struct parser {
+      template <typename P>
+      explicit parser(P const& p) : holder_(std::make_unique<holder<P>>(p)) {}
+
+      template <typename P>
+      explicit parser(P&& p) : holder_(std::make_unique<holder<P>>(std::move(p))) {}
+
+      parser(parser const& p) : holder_(p.holder_->copy()) {}
+      parser(parser&& p) : holder_(std::move(p.holder_)) {}
+
+      parser& operator=(parser const& p) {
+        holder_ = p.holder_->copy();
+        return *this;
+      }
+      parser& operator=(parser&& p) {
+        holder_ = std::move(p.holder_);
+        return *this;
+      }
+
+      ~parser() = default;
+
+      parse_result<Result, Stream> operator()(Stream& s) const {
+        return (*holder_)(s);
+      }
+
+    private:
+      struct holder_base {
+        virtual parse_result<Result, Stream> operator()(Stream&) const = 0;
+        virtual ~holder_base() = default;
+
+        virtual std::unique_ptr<holder_base> copy() const = 0;
+      };
+
+      template <typename P>
+      struct holder : holder_base {
+        holder(P const& p) : parser(p) {}
+        holder(P&& p) : parser(std::move(p)) {}
+
+        ~holder() = default;
+
+        parse_result<Result, Stream> operator()(Stream& s) const override {
+          return parser(s);
+        }
+
+        std::unique_ptr<holder_base> copy() const override {
+          return std::make_unique<holder>(parser);
+        }
+      private:
+        P parser;
+      };
+
+      std::unique_ptr<holder_base> holder_;
+    };
+
+  } // namespace combix
+} // namespace coco
+
+#endif
---
+++ test/coco/combix/parser_test.cpp
@@ -0,0 +1,49 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE expected
+
+#include <coco/combix/iterator_stream.hpp>
+#include <coco/combix/primitives.hpp>
+#include <coco/combix/combinators.hpp>
+#include <coco/combix/parser.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+BOOST_AUTO_TEST_SUITE(combix)
+BOOST_AUTO_TEST_SUITE(parser)
+
+  BOOST_AUTO_TEST_CASE(any) {
+    using stream_type = coco::combix::iterator_stream<std::string::const_iterator>;
+
+    auto const src = std::string{"test"};
+    auto s = coco::combix::iter_stream(std::begin(src), std::end(src));
+    auto const p = coco::combix::any();
+    auto const wrapped = coco::combix::parser<char, stream_type>(p);
+
+    BOOST_TEST(wrapped(s).unwrap() == 't');
+    BOOST_TEST(wrapped(s).unwrap() == 'e');
+    BOOST_TEST(wrapped(s).unwrap() == 's');
+    BOOST_TEST(wrapped(s).unwrap() == 't');
+    BOOST_TEST(wrapped(s).is_error());
+  }
+
+  BOOST_AUTO_TEST_CASE(complicated) {
+    using stream_type = coco::combix::iterator_stream<std::string::const_iterator>;
+
+    auto const src = std::string{"a1B"};
+    auto s = coco::combix::iter_stream(std::begin(src), std::end(src));
+    auto const alpha = coco::combix::parser<char, stream_type>(
+        coco::combix::satisfy([](auto&& c) { return 'a' <= c && c <= 'z'; }));
+    auto const num = coco::combix::parser<char, stream_type>(
+        coco::combix::satisfy([](auto&& c) { return '0' <= c && c <= '9'; }));
+    auto const choice = coco::combix::parser<char, stream_type>(
+        coco::combix::choice(alpha, num));
+
+    BOOST_TEST(choice(s).unwrap() == 'a');
+    BOOST_TEST(choice(s).unwrap() == '1');
+    BOOST_TEST(choice(s).is_error());
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
