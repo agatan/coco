6bedaa053cb313c24850228525be1e79dcd94357
agatan <m.k.mahoo@gmail.com>
2016-04-27 14:32:34 +0900

    Add unified header

---
+++ include/coco/combix.hpp
@@ -0,0 +1,14 @@
+#ifndef COCO_COMBIX_HPP_
+#define COCO_COMBIX_HPP_
+
+#include <coco/combix/parse_result.hpp>
+#include <coco/combix/stream_traits.hpp>
+#include <coco/combix/iterator_stream.hpp>
+#include <coco/combix/parser_traits.hpp>
+#include <coco/combix/primitives.hpp>
+#include <coco/combix/lazy_fun.hpp>
+#include <coco/combix/parser.hpp>
+#include <coco/combix/combinators.hpp>
+#include <coco/combix/chars.hpp>
+
+#endif
