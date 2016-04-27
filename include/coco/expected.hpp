c44740f8c0716475fd892c29fb14163b45482ee1
agatan <m.k.mahoo@gmail.com>
2016-04-27 13:57:12 +0900

    Fix expected constructor

--- include/coco/expected.hpp
+++ include/coco/expected.hpp
@@ -50,10 +50,8 @@ namespace coco {
     ~expected() = default;
 
     // constructors
-    template <typename T2,
-              typename std::enable_if<std::is_convertible<T2, T>::value,
-                                      std::nullptr_t>::type = nullptr>
-    expected(T2 const& v) : value(static_cast<T>(v)) {}
+    expected(T const& v) : value(v) {}
+    expected(T&& v) : value(std::move(v)) {}
 
     expected(E const& e): value(detail::error_holder<E>{e}) {}
     expected(E&& e): value(detail::error_holder<E>{std::move(e)}) {}
